83256bcd1ae53daf655d094191b8314a45b6bf7c
blaiseludvig <blaiseludvig@gmail.com>
2022-09-23 22:39:29 +0200

    gombos gyakorlás hozzáadása

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
 
 console.log("javascript betöltve!");
+
+let gomb_kattints = document.getElementById("gomb-kattints");
+let gratulalok = document.getElementById("gratulalok");
+let gomb_bezar = document.getElementById("gomb-bezar");
+
+gomb_kattints.addEventListener("click", function () {
+    gratulalok.classList.remove("d-none");
+})
+
+gomb_bezar.addEventListener("click", function () {
+    gratulalok.classList.add("d-none");
+})
